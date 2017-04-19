c516d3712b6ad5d2ded7307dba49db8165fd134e
Mummi <mulle@salameh.dk>
2017-04-19 10:46:12 +0200

    new class

---
+++ src/test/java/exercise/UserFacadeTestIT.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import exercise.fakedatabase.UserFacadeFake;
+import exercise.realdatabase.UserFacadeRealDB;
+
+/**
+ * Created by MJPS on 19/04/2017.
+ */
+public class UserFacadeTestIT extends UserFacadeTest{
+	
+	@Override
+	public IUserFacade makeUserFacade() {
+		return new UserFacadeRealDB("pu_localDB");
+	}
+}
